24c50d18cca60cb55b99086c9f73be06e97774f2
Novikov Andrey <nyb4eg@gmail.com>
2022-01-12 20:56:36 +0300

    Lesson 1. Code style fix

---
+++ usersapp/models.py
@@ -0,0 +1,12 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+    birthday = models.DateField(null=True, blank=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid4())
