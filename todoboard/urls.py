9d321a8be1db520fc2ac13048179c5b8beae53fc
Andrey_Novikov <81187863+NyB4eG@users.noreply.github.com>
2022-01-24 21:56:49 +0300

    Lesson 3 (#4)

--- todoboard/urls.py
+++ todoboard/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from todoapp.views import ProjectModelViewSet, TODOModelViewSet
 from usersapp.views import UserModelViewSet
 
 router = DefaultRouter()
 router.register("users", UserModelViewSet)
+router.register("projects", ProjectModelViewSet)
+router.register("TODO", TODOModelViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
