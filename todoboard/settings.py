0dd9a4a7cfc298ad290c29d24bfd3433f5bda03c
Novikov Andrey <nyb4eg@gmail.com>
2022-02-21 22:10:48 +0300

    Lesson 12

--- todoboard/settings.py
+++ todoboard/settings.py
@@ -87,11 +87,17 @@ WSGI_APPLICATION = "todoboard.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(os.path.join(BASE_DIR, "db.sqlite3")),
-    }
+	'default': {
+    	    'ENGINE': 'django.db.backends.postgresql',
+    	    'NAME': 'REST',
+    	    'USER': 'novikov',
+    	    'PASSWORD': '7135121',
+    	    'HOST': 'db',
+    	    'PORT': '5432',
+	}
 }
+
+
 
 
 # Password validation
